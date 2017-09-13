a7c63591f604c6bad22045c6772ae85ecb043b1d
Administrator <https://silasIT@bitbucket.org/silasIT/success_stories.git>
2017-09-13 10:54:22 +0530

    intital commit 2nd commint

--- app/src/main/java/com/student/gameapplication/GameApplicationActivity.java
+++ app/src/main/java/com/student/gameapplication/GameApplicationActivity.java
@@ -9,5 +9,7 @@ public class GameApplicationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_application);
+
+        //yes i pushed
     }
 }
